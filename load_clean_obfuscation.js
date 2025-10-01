7e3e8308791cfc9e44b2b6d8c35f9ce0b5844d1f
kazakovlime <kazakov.aa@lime-shop.com>
2025-10-01 18:10:52 +0300

    Update load_clean_obfuscation.js

--- load_clean_obfuscation.js
+++ load_clean_obfuscation.js
@@ -1,4 +1,1 @@
-om.common.modelInfo().exportObfuscationState().setPath('state.zip').export();
 
-const fileId = om.filesystems.local().makeGlobalFile('state', 'zip', 'state.zip');
-om.common.resultInfo().addFileHash(fileId);
